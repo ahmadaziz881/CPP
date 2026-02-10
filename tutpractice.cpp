bb893d523bccd69b62ab1bb8ce80a493549cf6df
Ahmad Aziz <ahmadazizkhan881@gmail.com>
2026-02-09 23:57:59 -0800

    C++ practice to conditional statment

---
+++ tutpractice.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+/*its hello world code for cpp*/   
+
+       cout<<"Hello World!"<<endl;
+
+/*its cpp assignment in which sum two numbers*/
+
+int a;
+int b;
+       cout<<"Enter the number a"<<endl;
+       cin>>a;
+
+    cout<<"Enter the number b"<<endl;
+    cin>>b;
+
+int d= a+b;
+        cout<<"d="<<(a+b);
+
+/*if-else coding for c++*/
+
+int a;
+        cout<<"Enter the number a"<<endl;
+        cin>>a;
+
+if (a%2 == 0)
+{
+      cout<<"Even number"<<endl;
+}  else{
+      cout<<"Odd number"<<endl;
+}
+
+    return 0;
+}
