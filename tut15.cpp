e1cae965d3847afea9662fd3b01ece8e9d25bb22
Ahmad Aziz <ahmadazizkhan881@gmail.com>
2026-02-12 09:05:24 -0800

    structures and unions and enums

--- tut15.cpp
+++ tut15.cpp
@@ -1,24 +1,40 @@
 #include<iostream>
 using namespace std;
 
-    struct student
-    {
-         int id;
-         char   character;
-         float cgpa;
-    };
+   struct student
+   {
+        int id;
+        char   character;
+        float cgpa;
+   };
     
 
 int main(){
 
-    struct student Ahmad;
-    Ahmad.id=372974929;
-    Ahmad.character='S';
-    Ahmad.cgpa=3.2;
-        
+    
+
+     struct student Ahmad;
+     Ahmad.id=372974929;
+     Ahmad.character='S';
+     Ahmad.cgpa=3.2;
+ 
             cout<<Ahmad.id<<endl;
             cout<<Ahmad.character<<endl;
             cout<<Ahmad.cgpa<<endl;    
+
+
+    // unions in C++
+     union money ;
+     int rice;
+     char car;
+     float pounds;
+      rice=75;
+      cout<<rice<<endl;
+
+    // //Enums in C++ 
+     enum meal {breakfast , lunch , dinner};
+     cout<<breakfast<<endl;
+     cout<<lunch<<endl;
 
         return 0;
 }
